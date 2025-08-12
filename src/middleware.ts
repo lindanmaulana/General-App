3608591cae4e15a9fde384e3b6d22d3c1c6e6411
Lindan Maulana <lindanstemga@gmail.com>
2025-08-12 11:30:42 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,27 +16,31 @@ export default async function middleware(req: NextRequest) {
 
   if(isApiRoute) return NextResponse.next();
 
-  if(isLoggedIn) {
-    if(token) {
-      if(!token.exp || Date.now() > token.exp * 1000) {
-          const response = NextResponse.redirect(new URL("/auth/login", nextUrl))
-
-            response.cookies.set("authjs.session-token", "", { maxAge: 0 });
-            response.cookies.set("__Secure-next-auth.session-token", "", { maxAge: 0 });
-            response.cookies.set("next-auth.csrf-token", "", { maxAge: 0 });
-
-          return response
-      }
-
-      if(isAdminRoute && token.role !== "ADMIN") return NextResponse.redirect(new URL(REDIRECT_HOME, nextUrl), 307)
+  if(!isLoggedIn) {
+    if(!isPublicRoute) {
+      return NextResponse.redirect(new URL(REDIRECT_AUTH, nextUrl))
     }
 
-    if(!token) return NextResponse.redirect(new URL(REDIRECT_AUTH, nextUrl))
+    return NextResponse.next()
   }
 
-  if(!isLoggedIn && !isPublicRoute) return NextResponse.redirect(new URL(REDIRECT_AUTH, nextUrl))
+  if(isLoggedIn && token) {
+    if (!token.exp || Date.now() > token.exp * 1000) {
+      const response = NextResponse.redirect(new URL("/auth/login", nextUrl));
+      
+      response.cookies.set("authjs.session-token", "", { maxAge: 0 });
+      response.cookies.set("__Secure-next-auth.session-token", "", { maxAge: 0 });
+      response.cookies.set("next-auth.csrf-token", "", { maxAge: 0 });
+      
+      return response;
+    }
 
-  return NextResponse.next();
+    if(isAdminRoute && token.role !== "ADMIN") return NextResponse.redirect(new URL(REDIRECT_HOME, nextUrl))
+
+    return NextResponse.next()
+  }
+
+  return NextResponse.next()
 }
 
 export const config = {
