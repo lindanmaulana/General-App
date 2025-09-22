7bacee1ecadd6bfdcd22d424b63c9d1a62d12c3b
Lindan Maulana <lindanstemga@gmail.com>
2025-09-22 13:21:47 +0700

    refactor: action in hero banner

--- src/app/(home)/_components/sections/hero-banner/ActionHeroBanner.tsx
+++ src/app/(home)/_components/sections/hero-banner/ActionHeroBanner.tsx
@@ -8,7 +8,7 @@ export const ActionHeroBanner = () => {
     return (
         <Div initial="initial" animate="animate" variants={variantsContainerFadeUpComponents} className="flex items-center gap-6 py-4">
             <Div variants={variantsChildrenFadeUp}>
-                <Button className="dark:bg-white dark:hover:bg-white/80 dark:text-black !px-8 py-6 text-white font-semibold shadow-xl bg-gnrPrimary text-lg hover:bg-gnrPrimary/80 hover:scale-105 cursor-pointer transition-global duration-300" asChild>
+                <Button className="dark:bg-white dark:hover:bg-white/80 dark:text-black md:!px-8 md:py-6 text-white font-semibold shadow-xl bg-gnrPrimary text-lg hover:bg-gnrPrimary/80 hover:scale-105 cursor-pointer transition-global duration-300" asChild>
                     <Link href={"/dashboard/login"}>Mulai Sekarang <ArrowRight className="mt-1" /></Link>
                 </Button>
             </Div>
@@ -16,7 +16,7 @@ export const ActionHeroBanner = () => {
             <Div variants={variantsChildrenFadeUp} transition={{duration: 0.8, delay: 0.8}}>
                 <Button
                     variant={"outline"}
-                    className="dark:bg-transparent dark:text-white dark:border-white/10 !px-8 py-6 font-semibold shadow-xl bg-transparent text-lg cursor-pointer"
+                    className="dark:bg-transparent dark:text-white dark:border-white/10 md:!px-8 md:py-6 font-semibold shadow-xl bg-transparent text-lg cursor-pointer"
                     asChild
                 >
                     <Link href={"/demo"}>Lihat Demo</Link>
